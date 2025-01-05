879f838665de97c5dab2ea7c1f7c818f549f26db
Jithu Prabhakar <join.jitz@gmail.com>
2025-01-05 12:24:21 +0530

    sets up router for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import './App.css'
 import Register from './pages/Register'
 
 function App() {
   return (
-    <div>
-      <Register />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Register />} />
+      </Routes>
+    </Router>
   )
 }
 
