0c34ac2d03523c1926256b5071d2f508084da90b
Jithu Prabhakar <join.jitz@gmail.com>
2025-01-07 11:17:47 +0530

    adds more pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import './App.css'
 import Register from './pages/Register'
 import Home from './pages/Home'
 import Login from './pages/Login'
+import Devs from './pages/Devs'
+import Dashboard from './pages/Dashboard'
+import Leaderboard from './pages/Leaderboard'
+import CreateTeam from './pages/CreateTeam'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/leaderboard' element={<Leaderboard />} />
+        <Route path='/createteam' element={<CreateTeam />} />
         <Route path='/signup' element={<Register />} />
         <Route path='/login' element={<Login />} />
-        {/* Just a sample comment */}
+        <Route path='/humans' element={<Devs />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/Devs.jsx
@@ -0,0 +1,9 @@
+const Devs = () => {
+  return (
+    <div>
+      <h2>Created by Daniel T Gilbert and Jithu Prabhakar</h2>
+    </div>
+  )
+}
+
+export default Devs
